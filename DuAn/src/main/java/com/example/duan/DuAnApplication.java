0e33d66c6835cf89df54a634c85665bfea899b5a
khanhvan5 <vannkpc00390@gmail.com>
2026-03-09 14:57:20 +0700

    quanly
    quanly

--- DuAn/src/main/java/com/example/duan/DuAnApplication.java
+++ DuAn/src/main/java/com/example/duan/DuAnApplication.java
@@ -2,12 +2,15 @@ package com.example.duan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DuAnApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DuAnApplication.class, args);
+
     }
 
 }
